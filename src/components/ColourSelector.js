d98713567bd834955b202e171bd29a99f1862431
Yash708262 <yash708262@gmail.com>
2025-09-02 16:20:22 +0530

    Update ColourSelector.js

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -3,15 +3,7 @@ import React from "react";
 function ColourSelector({ colorConfig, selectColor }) {
   return (
     <div>
-      {colorConfig.map((item, index) => (
-        <button
-          key={index}
-          onClick={() => selectColor(item.color)}
-          style={{ marginRight: "10px" }}
-        >
-          {item.color}
-        </button>
-      ))}
+     
     </div>
   );
 }
