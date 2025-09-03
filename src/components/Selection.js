71806db0765c5cc38995cd44629deed2c3688b37
Yash708262 <yash708262@gmail.com>
2025-09-03 09:58:31 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,14 @@
 
+import React from 'react'
+
+const Selection = ({ color }) => {
+  return (
+     <div>
+      <div className="fix-box" style={{ backgroundColor: color }}></div>
+      <div className="fix-box" style={{ backgroundColor: color }}></div>
+      <div className="fix-box" style={{ backgroundColor: color }}></div>
+    </div>
+  )
+}
+
+export default Selection
