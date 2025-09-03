2d968e8a56bf1884d0903898c22af4e17dfc8997
Yash708262 <yash708262@gmail.com>
2025-09-03 10:47:12 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,35 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Selection = ({ color }) => {
   const [boxColors, setBoxColors] = useState([
-    "rgb(239, 239, 239)", // Box 1 default grey
-    "rgb(239, 239, 239)", // Box 2
-    "rgb(239, 239, 239)"  // Box 3
+    "rgb(239, 239, 239)",
+    "rgb(239, 239, 239)",
+    "rgb(239, 239, 239)"
   ]);
 
   const changeBoxColor = (index) => {
     const newColors = [...boxColors];
-    newColors[index] = color;  // sirf jis box pe click hua uska color change hoga
+    newColors[index] = color;
     setBoxColors(newColors);
   };
 
   return (
-    <div>
-      <div
-        className="fix-box"
-        style={{ backgroundColor: boxColors[0], width: "100px", height: "100px", margin: "5px" }}
-        onClick={() => changeBoxColor(0)}
-      ></div>
-      <div
-        className="fix-box"
-        style={{ backgroundColor: boxColors[1], width: "100px", height: "100px", margin: "5px" }}
-        onClick={() => changeBoxColor(1)}
-      ></div>
-      <div
-        className="fix-box"
-        style={{ backgroundColor: boxColors[2], width: "100px", height: "100px", margin: "5px" }}
-        onClick={() => changeBoxColor(2)}
-      ></div>
+    <div style={{ display: "flex", gap: "10px" }}>
+      {boxColors.map((boxColor, index) => (
+        <div
+          key={index}
+          className="fix-box"
+          style={{
+            backgroundColor: boxColor,
+            width: "100px",
+            height: "100px",
+            border: "1px solid black",
+          }}
+          onClick={() => changeBoxColor(index)}
+        ></div>
+      ))}
     </div>
   );
 };
