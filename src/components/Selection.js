f1d9c68070c4f269ebcc7ebeff540ed344b318a0
Yash708262 <yash708262@gmail.com>
2025-09-03 10:35:37 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,21 +1,35 @@
-import React from "react";
-
-const boxStyle = {
-  width: "100px",
-  height: "100px",
-  margin: "10px",
-  border: "1px solid black",
-  display: "inline-block",
-};
+import React, { useState } from 'react';
 
 const Selection = ({ color }) => {
-  const appliedColor = color || "rgb(239, 239, 239)";
+  const [boxColors, setBoxColors] = useState([
+    "rgb(239, 239, 239)", // Box 1 default grey
+    "rgb(239, 239, 239)", // Box 2
+    "rgb(239, 239, 239)"  // Box 3
+  ]);
+
+  const changeBoxColor = (index) => {
+    const newColors = [...boxColors];
+    newColors[index] = color;  // sirf jis box pe click hua uska color change hoga
+    setBoxColors(newColors);
+  };
 
   return (
     <div>
-      <div className="fix-box" style={{ ...boxStyle, backgroundColor: appliedColor }}></div>
-      <div className="fix-box" style={{ ...boxStyle, backgroundColor: appliedColor }}></div>
-      <div className="fix-box" style={{ ...boxStyle, backgroundColor: appliedColor }}></div>
+      <div
+        className="fix-box"
+        style={{ backgroundColor: boxColors[0], width: "100px", height: "100px", margin: "5px" }}
+        onClick={() => changeBoxColor(0)}
+      ></div>
+      <div
+        className="fix-box"
+        style={{ backgroundColor: boxColors[1], width: "100px", height: "100px", margin: "5px" }}
+        onClick={() => changeBoxColor(1)}
+      ></div>
+      <div
+        className="fix-box"
+        style={{ backgroundColor: boxColors[2], width: "100px", height: "100px", margin: "5px" }}
+        onClick={() => changeBoxColor(2)}
+      ></div>
     </div>
   );
 };
