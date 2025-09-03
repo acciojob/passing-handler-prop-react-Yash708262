cd5e1896e76afbcb99a907592aef0b2ee95026e4
Yash708262 <yash708262@gmail.com>
2025-09-03 09:58:09 +0530

    Update ColourSelector.js

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react'
 
-function ColourSelector({ colorConfig, selectColor }) {
+const ColourSelector = ({ setColor }) => {
   return (
     <div>
-     
+      <button onClick={() => setColor('red')}>Red</button>
+      <button onClick={() => setColor('green')}>Green</button>
+      <button onClick={() => setColor('blue')}>Blue</button>
     </div>
-  );
+  )
 }
 
-export default ColourSelector;
+export default ColourSelector
