e3125288bc741e37b339896aaf3a6be5a8200213
Yash708262 <yash708262@gmail.com>
2025-09-03 09:57:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,15 @@
-import React, { useState } from "react";
-import ColourSelector from "./ColourSelector";
-import Selection from "./Selection";
+import React, { useState } from 'react'
+import Selection from './Selection';
+import ColourSelector from './ColourSelector';
 
-const colorConfig = [
-  { color: "red" },
-  { color: "blue" },
-  { color: "green" },
-];
-
-function App() {
-  const [selectedColor, setSelectedColor] = useState("");
-
+const App = () => {
+  const [color, setColor] = useState();
   return (
     <div>
-      <h1>Color Picker</h1>
-      <ColourSelector
-        colorConfig={colorConfig}
-        selectColor={(color) => setSelectedColor(color)}
-      />
-
-      {/* Three selection boxes */}
-      <div style={{ display: "flex", gap: "10px", marginTop: "20px" }}>
-        <Selection selectedColor={selectedColor} />
-        <Selection selectedColor={selectedColor} />
-        <Selection selectedColor={selectedColor} />
-      </div>
+      <Selection color={color} />
+      <ColourSelector setColor={setColor} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
