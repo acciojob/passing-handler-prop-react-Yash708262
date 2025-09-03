3fed5206077a26a20b40056c8467b8635a43fc0c
Yash708262 <yash708262@gmail.com>
2025-09-03 10:15:35 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+const boxStyle = {
+  width: "100px",
+  height: "100px",
+  margin: "10px",
+  border: "1px solid black",
+  display: "inline-block"
+};
+
 const Selection = ({ color }) => {
   return (
     <div>
-      <div className="fix-box" style={{ backgroundColor: color }}></div>
-      <div className="fix-box" style={{ backgroundColor: color }}></div>
-      <div className="fix-box" style={{ backgroundColor: color }}></div>
+       <div className="fix-box" style={{ ...boxStyle, backgroundColor: color }}></div>
+      <div className="fix-box" style={{ ...boxStyle, backgroundColor: color }}></div>
+      <div className="fix-box" style={{ ...boxStyle, backgroundColor: color }}></div>
     </div>
   );
 };
