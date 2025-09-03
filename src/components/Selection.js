f4f96bd393486736c1c51273697678068b9fe697
Yash708262 <yash708262@gmail.com>
2025-09-03 10:25:03 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -5,7 +5,8 @@ const boxStyle = {
   height: "100px",
   margin: "10px",
   border: "1px solid black",
-  display: "inline-block"
+  display: "inline-block",
+  backgroundColor: "rgb(239, 239, 239)" 
 };
 
 const Selection = ({ color }) => {
