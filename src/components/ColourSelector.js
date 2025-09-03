d67fa223859ae587254a2d4853fcc4c91ef41c56
Yash708262 <yash708262@gmail.com>
2025-09-03 10:09:13 +0530

    Update ColourSelector.js

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -3,9 +3,9 @@ import React from 'react';
 const ColourSelector = ({ applyColor }) => {
   return (
     <div>
-      <button onClick={() => applyColor('red')}>Red</button>
-      <button onClick={() => applyColor('green')}>Green</button>
-      <button onClick={() => applyColor('blue')}>Blue</button>
+      <button data-testid="blue" onClick={() => applyColor('blue')}>Blue</button>
+      <button data-testid="orange" onClick={() => applyColor('orange')}>Orange</button>
+      <button data-testid="green" onClick={() => applyColor('green')}>Green</button>
     </div>
   );
 };
